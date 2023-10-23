add82db4ca2fb185e1e3bd19a066d01fa339b798
Aditya Jain <20io1004@mitsgwl.ac.in>
2023-10-23 23:17:34 +0530

    first commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,36 @@
+export const QuizData = [
+  {
+    question:
+      "Which of the following is a front-end framework for web development?",
+    options: ["React", "Django", "Express", "Flask"],
+    answer: 1,
+  },
+  {
+    question:
+      "What is the primary purpose of a database management system (DBMS)?",
+    options: [
+      "To play video games",
+      "To store, manage, and retrieve data",
+      "To send emails",
+      "To cook food",
+    ],
+    answer: 2,
+  },
+  {
+    question:
+      "Which programming language is often used for data analysis and scientific computing?",
+    options: ["Java", "C++", "Python", "Ruby"],
+    answer: 3,
+  },
+  {
+    question:
+      "Which of the following is a JavaScript library for building user interfaces?",
+    options: ["React", "Django", "Express", "Flask"],
+    answer: 1,
+  },
+  {
+    question: "Which of the following is a Python web framework?",
+    options: ["React", "Django", "Express", "Flask"],
+    answer: 2,
+  },
+];
